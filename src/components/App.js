da30ef433cb23a0a694be9eaf91749ab360a9b23
sbalayan1 <balayans2014@yahoo.com>
2022-10-12 19:27:13 -0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Header from "./Header";
 import MainContainer from "./MainContainer";
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [countArr, setCountArr] = useState([])
   const [stocks, setStocks] = useState([])
   const [portfolio, setPortfolio] = useState([])
   const [filterObj, setFilterObj] = useState({
@@ -14,9 +16,32 @@ function App() {
   })
 
   useEffect(() => {
-    fetch('http://localhost:3001/stocks')
+    fetch('http://localhost:3000/stocks')
     .then(res => res.json())
     .then(data => setStocks(data))
+  }, [])
+
+  useEffect(() => {
+    if (countArr.length > 5) {
+      setCountArr([])
+      return
+    }
+
+    if (count % 3 === 0 && countArr[countArr.length-1] !== count) {
+      setCountArr((state) => [...state, count])
+    }
+
+  
+  }, [count, countArr])
+
+  const displayCountArr = useMemo(() => countArr.map(num => <li key={num}>{num}</li>), [countArr])
+  const handleIncreaseCount = useCallback(() => {
+    setCount(count+1)
+  }, [count])
+
+  const handleClearCount = useCallback(() => {
+    setCount(0)
+    setCountArr([])
   }, [])
 
   const handleAddStock = useCallback(stock => {
@@ -37,13 +62,24 @@ function App() {
     }).sort((a,b) => {
       if (filterObj.sort === 'Alphabetically') return a.name.localeCompare(b.name)
       if (filterObj.sort === 'Price') return a.price - b.price
+      return a
     })
   , [filterObj, stocks])
 
   return (
     <div>
       <Header />
+      <h2>
+        {count}
+      </h2>
+      <ul>
+        countArray: {displayCountArr}
+      </ul>
+      <button onClick={handleIncreaseCount}>increase count</button>
+      <button onClick={handleClearCount}>clear count</button>
       <MainContainer stocks={stocksToDisplay} portfolio={portfolio} handleAddStock={handleAddStock} handleRemoveStock={handleRemoveStock} filterStocks={filterStocks}/>
+      {count}
+      {countArr}
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+function returnFirstTwo() {
+
+}
+
+function returnLastTwo() {
+
+}
+
+function selectDiffDrivers(drivers, func) {
+
+}
+
